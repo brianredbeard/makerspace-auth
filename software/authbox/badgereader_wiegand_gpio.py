16fe1195621cebb9e70d07d8f622206258fd7216
Brian 'Redbeard' Harrington <redbeard@dead-city.org>
2018-12-05 11:04:54 -0800

    wiegand: Add Wiegand GPIO support
    This class adds support for Wiegand GPIO readers. Thusfar it has been
    tested with multiple EM41XX based readers.  The next steps will be to
    validate it with other units (e.g. HID Proxkey based readers, etc).
    
    To use this define a badge reader as follows in your INI file where the
    second argument is the GPIO pin which the Wiegand D0 pin is connected to
    and the third argument is the GPIO pin which the Wiegand D1 pin is
    connected to:
    
    ```
    badge_reader = WiegandGPIOReader:3:5
    ```
    
    The callback for this device returns a 4 byte (8 character), zero padded
    hexidecimal string capable of encoding up to 32 bits.  Most commodity
    EM41XX readers can present either 26 or 34 bits of data _with the first
    and last bit being used for parity_, resulting in 24/32 usable bits of
    data respectively.  This may be increased in the future to support 40
    bit readers, etc.

--- software/authbox/badgereader_wiegand_gpio.py
+++ software/authbox/badgereader_wiegand_gpio.py
@@ -15,15 +15,31 @@
 """Wiegand based badge reader directly connected via GPIO
 """
 
-from authbox.api import BasePinThread, GPIO
-import bitstring
+from authbox.api import BaseDerivedThread, GPIO
+import time
 
-bits = bitstring.BitStream('0x0')
+bits = ''
 t = 15
 timeout = t
 
+# This new base class has been created because we need multiple input pins in use
+# simultaneously.
+class BaseGPIOPinThread(BaseDerivedThread):
+  def __init__(self, event_queue, config_name, d0_pin, d1_pin, initial_output=GPIO.LOW):
+    super(BaseGPIOPinThread, self).__init__(event_queue, config_name)
 
-class WiegandGPIOReader(BasePinThread):
+    self.d0_pin = d0_pin
+    self.d1_pin = d1_pin
+
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setwarnings(False)  # for reusing pins
+    if self.d0_pin:
+      GPIO.setup(self.d0_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    if self.d1_pin:
+      GPIO.setup(self.d1_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+
+class WiegandGPIOReader(BaseGPIOPinThread):
   """Badge reader hardware abstraction.
 
   A Wiegand GPIO badge reader is defined in config as:
@@ -32,14 +48,18 @@ class WiegandGPIOReader(BasePinThread):
     name = WiegandGPIOReader:3:5
 
   where 3 is the D0 pin (physical numbering), and 5 is the D1 pin (also 
-  hysical numbering).
+  physical numbering).
   """
-  def __init__(self, event_queue, config_name, d0_pin, d1_pin, bit_len=26, on_scan=None):
-    super(WiegandGPIOReader, self).__init__(event_queue, config_name)
+  def __init__(self, event_queue, config_name, d0_pin, d1_pin, on_scan=None):
+    # presently, i've dropped bit_len as a parameter.  likely i'm going to
+    # swap this around to make it so that you can pass a decoding format to
+    # make it easier to work with different encoding schemes.
+    super(WiegandGPIOReader, self).__init__(event_queue, config_name,
+            int(d0_pin), int(d1_pin))
     self._on_scan = on_scan
     if self._on_scan:
-        GPIO.add_event_detect(d0_pin, GPIO.FALLING, callback=self._callback)
-        GPIO.add_event_detect(d1_pin, GPIO.FALLING, callback=self._callback)
+        GPIO.add_event_detect(self.d0_pin, GPIO.FALLING, callback=self.decode)
+        GPIO.add_event_detect(self.d1_pin, GPIO.FALLING, callback=self.decode)
 
 
   def _callback(self, unused_channel):
@@ -50,10 +70,17 @@ class WiegandGPIOReader(BasePinThread):
   def decode(self, channel):
       global bits
       global timeout
+      # This was originally done using the bitstream package, but we've
+      # switched back to this to minimize imports as well as calculate
+      # arbitrary bit boundaries in the future.  bitstream also made
+      # calculations more complex due to endianess issues.  When working
+      # with the readers directly it's easier to just extract the stream
+      # and then we can implement helper functions later to separate facility
+      # code and id.
       if channel == self.d0_pin:
-          bits.append('0b0')
+          bits = bits + "0"
       elif channel == self.d1_pin:
-          bits.append('0b1')
+          bits = bits + "1"
       timeout = t
 
 
@@ -69,24 +96,34 @@ class WiegandGPIOReader(BasePinThread):
     global bits
     global timeout
 
-    rfid = ''
-    print "About to read_input"
     while 1:
         if bits:
             timeout = timeout -1
             time.sleep(0.001)
             if len(bits) > 1 and timeout == 0:
-                #print "Binary:",bits
-                #result = int(str(bits[1:26]),2)
-                print('Bits is {} bits in length'.format(len(bits)))
-                print('{:012X}'.format(bits[-25:-1].int))
-                print('{0:30b}'.format(bits[-25:-1].int))
-                bits = bitstring.BitStream('0x0')
+                b = bits
+                # As we use a global because of how we operate with interrupts
+                # reset the value back when we are no longer grabbing input.
+                bits = ''
+                # With some wiegand readers it adds additional control parity
+                # at the front/back of the stream, which are not used in our
+                # code calculation, thus we need to strip the values
+                start = 0 - len(bits) + 1
+
+                # These are useful lines to expose for debugging problems in
+                # the system
+                #
+                #Print out a visualization of the bitstream
+                # print "Binary:",bits
+                # print out a calculation of the value
+                # print('Bits is {} bits in length'.format(len(bits)))
+                # print('{:012X}'.format(int(b[start:-1], 2)))
+                # print('{0:30b}'.format(int(b[start:-1], 2)))
+
+                return '{:08X}'.format(int(b[start:-1], 2))
         else:
             time.sleep(0.001)
 
-    print "Badge read:", rfid
-    return rfid
 
   def run_inner(self):
     line = self.read_input()
